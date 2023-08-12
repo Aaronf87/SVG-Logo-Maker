84fa74cb13521aa2bc90bbc56aef11814e70a46e
Aaron Flores <aaron_flores08@yahoo.com>
2023-08-12 17:24:39 -0500

    structured js file

--- index.js
+++ index.js
@@ -2,10 +2,10 @@
 // TODO: Include packages needed for this application
 const inquirer = require('inquirer');
 const fs = require('fs');
-const questions = require('./lib/questions');
-const Circle = require('./lib/circle')
-const Square = require('./lib/square')
-const Triangle = require('./lib/triangle')
+const questions = require('./lib/questions.js');
+const Circle = require('./lib/circle.js')
+const Square = require('./lib/square.js')
+const Triangle = require('./lib/triangle.js')
 
 // TODO: Create an array of questions for user input
  const init = () => {
@@ -21,22 +21,24 @@ shape = new Triangle()
 shape = new Square()
 }else {
   shape = new Circle()
-}
+};
+
 // TODO: Create a function to write README file
 
 
-const generateSvg = ({letters, textcolor, shapecolor, imageshape})=>
-`
-# ${letters} 
-`
+// const generateSvg = ({letters, textcolor, shapecolor, imageshape})=>
+// `
+// # ${letters} 
+// `
 
-const svgContent = generateSvg(answers);
+// const svgContent = generateSvg(answers);
 })
 function writeToFile(fileName, data) {
   
 
-  fs.writeFile('generateSvg.svg', svgContent, (err) =>
+  fs.writeFile('generateSvg.svg', shape, (err) =>
     err ? console.log(err) : console.log('Successfully created SVG logo!')
   );
 };};
+// a function call to start application
 init();
